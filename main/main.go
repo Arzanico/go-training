9156b1c7688dcad3b323d901149582cd07bfdb06
nicolas arzani domenget <arzanico@hotmail.com>
2026-02-21 20:44:59 -0300

    initial commit for the training

---
+++ main/main.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var animals = map[string]func(string) Animal{
+	"bird":  newBird,
+	"snake": newSnake,
+	"cow":   newCow,
+}
+
+var actions = map[string]func(Animal) string{
+	"eat":   eat,
+	"move":  move,
+	"speak": speak,
+}
+
+type Animal interface {
+	foodEaten() string
+	locomotion() string
+	SpokenSound() string
+	getName() string
+}
+
+func newAnimal(animalType string, name string) (Animal, error) {
+	a, ok := animals[strings.TrimSpace(strings.ToLower(animalType))]
+	if !ok {
+		return Animal(nil), fmt.Errorf("%s not recognized. Available options are: bird, snake and cow\n", animalType)
+	}
+	return a(name), nil
+}
+
+func eat(a Animal) string {
+	return a.foodEaten()
+}
+
+func move(a Animal) string {
+	return a.locomotion()
+}
+
+func speak(a Animal) string {
+	return a.SpokenSound()
+}
+
+type bird struct {
+	name string
+}
+
+func newBird(name string) Animal {
+	return bird{
+		name: name,
+	}
+}
+
+func (o bird) getName() string {
+	return o.name
+}
+
+func (o bird) foodEaten() string {
+	return fmt.Sprintf("worms")
+}
+
+func (o bird) locomotion() string {
+	return fmt.Sprintf("fly")
+}
+
+func (o bird) SpokenSound() string {
+	return fmt.Sprintf("peep")
+}
+
+type snake struct {
+	name string
+}
+
+func newSnake(name string) Animal {
+	return snake{
+		name: name,
+	}
+}
+
+func (o snake) getName() string {
+	return o.name
+}
+
+func (o snake) foodEaten() string {
+	return fmt.Sprintf("mice")
+}
+
+func (o snake) locomotion() string {
+	return fmt.Sprintf("slither")
+}
+
+func (o snake) SpokenSound() string {
+	return fmt.Sprintf("hsss")
+}
+
+type cow struct {
+	name string
+}
+
+func newCow(name string) Animal {
+	return cow{
+		name: name,
+	}
+}
+
+func (o cow) getName() string {
+	return o.name
+}
+
+func (o cow) foodEaten() string {
+	return fmt.Sprintf("grass")
+}
+
+func (o cow) locomotion() string {
+	return fmt.Sprintf("walk")
+}
+
+func (o cow) SpokenSound() string {
+	return fmt.Sprintf("moo")
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Hi, Welcome to Friend from the Farm")
+	fmt.Println("Here you can choose a set of animals from the list of available animal")
+	fmt.Println("You will be able to provide a name for each animal")
+	fmt.Println("After that you will be able to get some information from that animal")
+	fmt.Println("Usage: You have 3 commands:\n" +
+		"newAnimal" +
+		"query\n" +
+		"exit\n")
+	fmt.Println("newAnimal allow you to create a new animal, for this comand you need to provide a name and a type for the animals as follows" +
+		"newAnimal <name> <type>")
+	fmt.Println("query allows you to get animal information, for this comand you need to provide the name of the animal you want the information from and an action as follows" +
+		"query <name> action")
+	fmt.Println("THere are 3 abailable action for each animal\n" +
+		"1: eat\n" +
+		"2: move\n" +
+		"3: speak\n")
+	fmt.Println("the command <exit> will stop the program\n")
+
+	userAnimals := make(map[string]Animal)
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("unknown error", err)
+			return
+		}
+
+		input = strings.ToLower(strings.TrimSpace(input))
+		if input == "exit" {
+			fmt.Println("shutting down....")
+			return
+		}
+
+		var command, animalName, property string
+		if commands := strings.Split(input, " "); len(commands) < 3 {
+			fmt.Println("command nos valid, check your spelling")
+			continue
+		} else {
+			command = commands[0]
+			animalName = commands[1]
+			property = commands[2]
+		}
+
+		switch command {
+		case "newanimal":
+			a, newAnimalErr := newAnimal(property, animalName)
+			if newAnimalErr != nil {
+				fmt.Printf(" error %s", newAnimalErr.Error())
+				continue
+			}
+			userAnimals[animalName] = a
+			fmt.Println("Created it!")
+		case "query":
+			a, ok := userAnimals[animalName]
+			if !ok {
+				fmt.Printf("animal %s not found\n", animalName)
+				continue
+			}
+
+			acc, ok := actions[property]
+			if !ok {
+				fmt.Printf("action %s not found\n", property)
+				continue
+			}
+			fmt.Println(acc(a))
+		default:
+			continue
+
+		}
+
+	}
+
+}
---
+++ main/peers/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Animal interface {
+	Eat() string
+	Move() string
+	Speak() string
+}
+
+type Cow struct {
+	food       string
+	locomotion string
+	noise      string
+}
+
+type Bird struct {
+	food       string
+	locomotion string
+	noise      string
+}
+
+type Snake struct {
+	food       string
+	locomotion string
+	noise      string
+}
+
+func (cow Cow) Speak() string {
+	return cow.noise
+}
+
+func (cow Cow) Eat() string {
+	return cow.food
+}
+
+func (cow Cow) Move() string {
+	return cow.locomotion
+}
+
+func (bird Bird) Speak() string {
+	return bird.noise
+}
+
+func (bird Bird) Eat() string {
+	return bird.food
+}
+
+func (bird Bird) Move() string {
+	return bird.locomotion
+}
+
+func (snake Snake) Speak() string {
+	return snake.noise
+}
+
+func (snake Snake) Eat() string {
+	return snake.food
+}
+
+func (snake Snake) Move() string {
+	return snake.locomotion
+}
+
+var Animals map[string]Animal = map[string]Animal{}
+
+func newCow() Cow {
+	return Cow{
+		food:       "grass",
+		locomotion: "walk",
+		noise:      "moo",
+	}
+}
+
+func newBird() Bird {
+	return Bird{
+		food:       "worms",
+		locomotion: "fly",
+		noise:      "peep",
+	}
+}
+
+func newSnake() Snake {
+	return Snake{
+		food:       "mice",
+		locomotion: "slither",
+		noise:      "hsss",
+	}
+}
+
+func log_created() {
+	log.Print("Created it!")
+
+}
+
+func main() {
+	input_scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("\n> ")
+		input_scanner.Scan()
+		raw_input := input_scanner.Text()
+		inp := strings.Split(raw_input, " ")
+		if inp[0] == "newanimal" || inp[0] == "n" {
+			switch inp[2] {
+			case "cow":
+				Animals[inp[1]] = newCow()
+				log_created()
+			case "bird":
+				Animals[inp[1]] = newBird()
+				log_created()
+			case "snake":
+				Animals[inp[1]] = newSnake()
+				log_created()
+			}
+		} else {
+			if inp[0] == "query" || inp[0] == "q" {
+				req_animal := Animals[inp[1]]
+				if req_animal != nil {
+					switch inp[2] {
+					case "speak":
+						log.Println(req_animal.Speak())
+					case "move":
+						log.Println(req_animal.Move())
+					case "eat":
+						log.Println(req_animal.Eat())
+					}
+				} else {
+					log.Println("- BAD Query - Try again")
+				}
+			} else {
+
+				log.Println("- BAD Query - Try again")
+			}
+		}
+	}
+}
