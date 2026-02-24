066ff0b678b62960a985436a21579fcee4345f42
nicolas arzani domenget <arzanico@hotmail.com>
2026-02-23 23:45:05 -0300

    race condition explanation

--- main/course3/module2/main.go
+++ main/course3/module2/main.go
@@ -5,6 +5,16 @@ import (
 	"sync"
 )
 
+/*
+A race condition is a characteristic of a code base of a program on which the condition of two processes of
+the program compete trying to change the state of the same resource.
+In other words, a race condition appears when two process of a same program tries to modify the
+state of the same resource.
+Race conditions are also related with interleaving. The order
+of execution of the process, when they are cocurrent is not deterministic, so in race condition the final state
+of a resource that is access from, at least two cocurrent precess, is unpredictable.
+*/
+
 func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(race())
