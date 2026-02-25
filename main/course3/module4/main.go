d06e182f6074392bddf5c4fe7c7c1ad3b6a9bd29
nicolas arzani domenget <arzanico@hotmail.com>
2026-02-25 17:06:35 -0300

    dinning philosophers solution

--- main/course3/module4/main.go
+++ main/course3/module4/main.go
@@ -5,45 +5,85 @@ import (
 	"sync"
 )
 
-type Host struct {
-	guess1 sync.Mutex
-	guess2 sync.Mutex
-}
+var askForEatChannel = make(chan struct{})
 
-type ChopS struct {
+var permissionChannel = make(chan bool)
+
+var philoChannel = make(chan struct{})
+
+type chopStick struct {
 	sync.Mutex
 }
 
 type Philo struct {
-	leftCS, rightCS *ChopS
+	n               int
+	leftCs, rightCs *chopStick
 }
 
-func (o Philo) eat() {
-	for i := 0; i < 3; i++ {
-		o.leftCS.Lock()
-		o.rightCS.Lock()
-		fmt.Println("eating")
-		o.rightCS.Unlock()
-		o.leftCS.Unlock()
+func (o Philo) eat(wg *sync.WaitGroup) {
+	counter := 0
+	for {
+		if counter == 3 {
+			break
+		}
+		askForEatChannel <- struct{}{}
+		select {
+		case p := <-permissionChannel:
+			if p {
+				o.leftCs.Lock()
+				o.rightCs.Lock()
+				fmt.Printf("starting to eat %d\n", o.n)
+				fmt.Printf("finishing to eat %d\n", o.n)
+				o.rightCs.Unlock()
+				o.leftCs.Unlock()
+				philoChannel <- struct{}{}
+				counter++
+			}
+		}
 	}
+	wg.Done()
 }
 
 func main() {
-	cSticks := make([]*ChopS, 5)
+
+	cSticks := make([]*chopStick, 5)
 	for i := 0; i < 5; i++ {
-		cSticks[i] = new(ChopS)
+		cSticks[i] = new(chopStick)
 	}
 
-	philo := make([]*Philo, 5)
+	go host()
+
+	philo := make([]Philo, 5)
+	wg := sync.WaitGroup{}
+	wg.Add(5)
+
 	for i := 0; i < 5; i++ {
-		philo[i] = &Philo{
-			leftCS:  cSticks[i],
-			rightCS: cSticks[(i+1)%5],
+		philo[i] = Philo{
+			n:       i + 1,
+			leftCs:  cSticks[i],
+			rightCs: cSticks[(i+1)%5],
+		}
+
+		go philo[i].eat(&wg)
+	}
+	wg.Wait()
+
+}
+
+func host() {
+	counter := 0
+	for {
+
+		select {
+		case <-askForEatChannel:
+			if counter < 2 {
+				permissionChannel <- true
+				counter++
+			} else {
+				permissionChannel <- false
+			}
+		case <-philoChannel:
+			counter--
 		}
 	}
-
-	for i := 0; i < 5; i++ {
-		go philo[i].eat()
-	}
-
 }
