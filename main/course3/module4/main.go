26a58820cc3253d76de3b86ad556cc09bb3eee95
nicolas arzani domenget <arzanico@hotmail.com>
2026-02-27 16:19:08 -0300

    work in my own solution again

--- main/course3/module4/main.go
+++ main/course3/module4/main.go
@@ -5,144 +5,116 @@ import (
 	"sync"
 )
 
-// --- Chopsticks ---
+type seat struct{}
 
-type chopStick struct{ sync.Mutex }
-
-// --- Philosopher ---
-
-type Philo struct {
-	n               int
-	leftCs, rightCs *chopStick
+type chopStick struct {
+	sync.Mutex
 }
-
-// --- Host (runs in its own goroutine) ---
 
 type request struct {
-	reply chan struct{} // host closes/sends to grant permission
+	reply chan struct{}
 }
 
-type Host struct {
-	reqCh  chan request
-	doneCh chan struct{}
+type host struct {
+	requestSeatCh chan request
+	releaseSeatCh chan seat
 }
 
-func NewHost(maxConcurrent int) *Host {
-	h := &Host{
-		reqCh:  make(chan request),
-		doneCh: make(chan struct{}),
+func newHost() *host {
+	h := &host{
+		requestSeatCh: make(chan request),
+		releaseSeatCh: make(chan seat),
 	}
-	go h.run(maxConcurrent)
+
 	return h
 }
 
-func (h *Host) run(maxConcurrent int) {
-	eating := 0
-	var queue []request
+func (o *host) acquire() {
+	r := request{
+		reply: make(chan struct{}),
+	}
+	o.requestSeatCh <- r
+	<-r.reply
+}
+
+func (o *host) release() {
+	o.releaseSeatCh <- seat{}
+}
+
+func (o *host) run(maxConcurrent int) {
+
+	occupiedSeatCounter := 0
+	philosQueue := make([]request, 0)
 
 	for {
-		// If there is capacity and someone is queued, grant immediately.
-		if eating < maxConcurrent && len(queue) > 0 {
-			r := queue[0]
-			queue = queue[1:]
-			eating++
-			// Grant permission
-			r.reply <- struct{}{}
-			close(r.reply)
-			continue
+		if occupiedSeatCounter < maxConcurrent && len(philosQueue) > 0 {
+			p := philosQueue[0]
+			philosQueue = philosQueue[1:]
+
+			occupiedSeatCounter++
+
+			p.reply <- struct{}{}
+			close(p.reply)
 		}
 
 		select {
-		case r := <-h.reqCh:
-			if eating < maxConcurrent {
-				eating++
+		case r := <-o.requestSeatCh:
+			if occupiedSeatCounter < maxConcurrent {
+				occupiedSeatCounter++
 				r.reply <- struct{}{}
 				close(r.reply)
-			} else {
-				// queue the request until someone finishes
-				queue = append(queue, r)
 			}
-
-		case <-h.doneCh:
-			// One philosopher finished eating
-			if eating > 0 {
-				eating--
+			philosQueue = append(philosQueue, r)
+		case <-o.releaseSeatCh:
+			if occupiedSeatCounter > 0 {
+				occupiedSeatCounter--
 			}
 		}
 	}
 }
 
-func (h *Host) Acquire() {
-	r := request{reply: make(chan struct{})}
-	h.reqCh <- r
-	<-r.reply // wait until host grants permission
+type philosopher struct {
+	n               int
+	leftCS, rightCS *chopStick
 }
 
-func (h *Host) Release() {
-	h.doneCh <- struct{}{}
-}
-
-// --- Eating logic ---
-
-func (p Philo) eat(host *Host, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for meal := 0; meal < 3; meal++ {
-		// Ask permission from host (host goroutine enforces max 2 concurrent)
-		host.Acquire()
-
-		// Pick up chopsticks in any order (not lowest-numbered first).
-		// To keep it "any order", we alternate by philosopher number.
-		if p.n%2 == 0 {
-			p.rightCs.Lock()
-			p.leftCs.Lock()
-		} else {
-			p.leftCs.Lock()
-			p.rightCs.Lock()
-		}
-
-		// Print after obtaining necessary locks
-		fmt.Printf("starting to eat %d\n", p.n)
-
-		// (No sleep needed by the spec; keeping it minimal/deterministic.)
-
-		// Print before releasing locks
-		fmt.Printf("finishing eating %d\n", p.n)
-
-		// Put down chopsticks
-		p.rightCs.Unlock()
-		p.leftCs.Unlock()
-
-		// Tell host we're done
-		host.Release()
+func (o *philosopher) eat(h *host, wg *sync.WaitGroup) {
+	for i := 0; i < 3; i++ {
+		h.acquire()
+		o.leftCS.Lock()
+		o.rightCS.Lock()
+		fmt.Printf("start eating %d\n", o.n)
+		o.rightCS.Unlock()
+		o.leftCS.Unlock()
+		fmt.Printf("end eating %d\n", o.n)
+		i++
+		h.release()
 	}
+
+	wg.Done()
 }
 
 func main() {
-	// Create 5 chopsticks
-	cSticks := make([]*chopStick, 5)
+
+	chopSticks := make([]*chopStick, 5)
 	for i := 0; i < 5; i++ {
-		cSticks[i] = new(chopStick)
+		chopSticks[i] = new(chopStick)
 	}
 
-	// Host allows at most 2 philosophers to eat concurrently (in its own goroutine)
-	host := NewHost(2)
+	h := newHost()
+	h.run(2)
 
-	// Create 5 philosophers (1..5)
-	philos := make([]Philo, 5)
+	philosophers := make([]philosopher, 5)
+	philoWg := new(sync.WaitGroup)
 	for i := 0; i < 5; i++ {
-		philos[i] = Philo{
-			n:       i + 1,
-			leftCs:  cSticks[i],
-			rightCs: cSticks[(i+1)%5],
+		philosophers[i] = philosopher{
+			n:       i,
+			leftCS:  chopSticks[i],
+			rightCS: chopSticks[(i+1)%5],
 		}
+		go philosophers[i].eat(h, philoWg)
 	}
 
-	// Run philosophers
-	var wg sync.WaitGroup
-	wg.Add(5)
-	for i := 0; i < 5; i++ {
-		go philos[i].eat(host, &wg)
-	}
-	wg.Wait()
+	philoWg.Wait()
+
 }
